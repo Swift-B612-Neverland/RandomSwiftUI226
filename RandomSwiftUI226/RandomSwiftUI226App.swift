3a24455e14beeb99f9a230496ea3dc3898e9039b
Peter Pan <apppeterpan@gmail.com>
2025-09-09 14:18:24 +0800

    Initial Commit

---
+++ RandomSwiftUI226/RandomSwiftUI226App.swift
@@ -0,0 +1,17 @@
+//
+//  RandomSwiftUI226App.swift
+//  RandomSwiftUI226
+//
+//  Created by SHIH-YING PAN on 2025/9/9.
+//
+
+import SwiftUI
+
+@main
+struct RandomSwiftUI226App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
